ee30166c724576dc2466566fbc99422ebdc14bc3
OliveMwaura <olive.mwaura@strathmore.edu>
2024-06-13 12:17:56 +0300

    Search component works successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
-import { Hero } from "@/components"
+import { Hero, CustomFilter, SearchBar } from "@/components"
+
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x
+      padding-y max-width" id="discover"
+      >
+        <div className="home__text-container">
+          <h1 className="tex-4x1
+          font-extrabold">Car Catalogue</h1>
+          <p>
+            Explore the cars you might like
+          </p>
+        </div>
+        <div className="home_filters">
+          <SearchBar />
+
+          <div
+          className="home_filter-containner">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="fuel"/>
+
+          </div>
+        </div>
+
+
+      </div>
     </main>
   )
 }
